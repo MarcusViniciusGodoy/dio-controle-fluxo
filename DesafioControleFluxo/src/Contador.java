715852f92605cbdb8c2de541e0d2c61013bc4082
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-05-02 11:33:28 -0300

    Implementando a lógica

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -9,18 +9,20 @@ public class Contador {
 		int parametroDois = terminal.nextInt();
 		
 		try {
-			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
-		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
 		
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		for (int i = 1; i < contagem; i++){
+            System.out.println("Imprimindo o número " + i);
+        }
 	}
 }
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,3 +1,7 @@
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
 
 }
